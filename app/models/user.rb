5bed80d24d4132ac5712908da6f5a09ca3814ed8
Martin Stenflo <stenflo@gmail.com>
2020-10-20 12:07:53 -0700

    add user roles with rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :confirmable
+  :recoverable, :rememberable, :validatable, :trackable, :confirmable
+  
+  rolify
 
   def to_s
     email
@@ -13,4 +15,22 @@ class User < ApplicationRecord
   end
 
   has_many :courses
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:student) if self.roles.blank?
+  end
+
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.roles.blank?
+      self.add_role(:teacher)
+      self.add_role(:student)
+    else
+      self.add_role(:student) if self.roles.blank?
+      self.add_role(:teacher)
+    end
+  end
+  
 end
